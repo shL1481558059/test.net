6df274940a9f6bb3349a487152c2feea6babff93
chenlong.wang <chenlong.wang@unow.com.cn>
2023-03-30 10:56:31 +0800

    11

---
+++ testDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
