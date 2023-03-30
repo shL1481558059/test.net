3cb9339b543f08050705fdaddcd9d01e24617f98
chenlong.wang <chenlong.wang@unow.com.cn>
2023-03-30 11:16:43 +0800

    1

--- testDocker/Program.cs
+++ testDocker/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
 
 var app = builder.Build();
 
@@ -13,6 +14,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStaticFiles();
+
 app.UseHttpsRedirection();
 
 
